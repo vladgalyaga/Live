a5832e64210ccb7842d7fd24c3df4eb70c18d5b6
Владислав Галяга < >
2017-10-14 14:54:25 +0300

    Добавьте файлы проекта.

---
+++ Live/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Live.Startup))]
+namespace Live
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
